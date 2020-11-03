9f9b4ec9630926638a066bee288987503c1efece
Nick Lanam <nick.lanam@tekraq.com>
2020-11-02 19:27:31 -0800

    Normal ordering of alt and gui keys, move caps

--- keyboards/moonlander/keymaps/nester-two/keymap.c
+++ keyboards/moonlander/keymaps/nester-two/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRACKET,                    KC_RBRACKET,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_EQUAL,       
     KC_BSPACE,      KC_A,           LSFT_T(KC_S),   LGUI_T(KC_D),   KC_F,           KC_G,           KC_LPRN,                        KC_RPRN,        KC_H,           KC_J,           RGUI_T(KC_K),   RSFT_T(KC_L),   KC_SCOLON,      KC_DELETE,      
     KC_LSHIFT,      LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,                                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,      
-    KC_LCTRL,       KC_LALT,        KC_LGUI,        KC_F11,         LALT_T(KC_QUOTE),               TO(5),                          TO(4),                          LALT_T(KC_GRAVE),KC_F12,        KC_RGUI,        KC_RALT,        KC_RCTRL,       
+    KC_LCTRL,       KC_LGUI,        KC_LALT,        KC_F11,         LALT_T(KC_QUOTE),               TO(5),                          TO(4),                          LALT_T(KC_GRAVE),KC_F12,        KC_RALT,        KC_RGUI,        KC_RCTRL,       
                                                                     LT(2,KC_SPACE), LT(3,KC_ENTER), TT(2),                          TT(3),          LT(3,KC_ENTER), LT(2,KC_SPACE)
   ),
   /*
@@ -39,16 +39,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_GAME] = LAYOUT_moonlander(
     _______,        _______,        _______,        _______,        _______,        _______,        TO(0),                          TG(4),          _______,        _______,        _______,        _______,        _______,        _______,        
     _______,        _______,        _______,        _______,        _______,        _______,        SH_OFF,                         SH_OFF,         _______,        _______,        _______,        _______,        _______,        _______,        
-    _______,        _______,        KC_S,           KC_D,           KC_F,           _______,        SH_OS,                          XXXXXXX,        _______,        KC_J,           KC_K,           KC_L,           _______,        _______,        
+    _______,        _______,        KC_S,           KC_D,           KC_F,           _______,        SH_OS,                          SH_OS,          _______,        KC_J,           KC_K,           KC_L,           _______,        _______,        
     _______,        KC_Z,           _______,        _______,        _______,        _______,                                                        _______,        _______,        _______,        _______,        KC_SLASH,       _______,        
-    _______,        _______,        XXXXXXX,        _______,        KC_LALT,                        TO(0),                          TO(0),                          KC_LALT,        _______,        XXXXXXX,        _______,        _______,        
+    _______,        XXXXXXX,        _______,        _______,        KC_LALT,                        TO(0),                          TO(0),                          KC_LALT,        _______,        _______,        XXXXXXX,        _______,        
                                                                     KC_SPACE,       KC_ENTER,       MO(2),                          MO(3),          KC_ENTER,       KC_SPACE
   ),
   /*
    * Symbols on left hand, numbers on right hand
    */
   [LAYER_SYM_NUM] = LAYOUT_moonlander(
-    TO(0),          XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MUTE,                        KC_MNXT,        XXXXXXX,        KC_NUMLOCK,     KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,    _______,        
+    TO(0),          XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_MUTE,                        KC_MNXT,        KC_CAPSLOCK,    KC_NUMLOCK,     KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,    _______,        
     _______,        KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         XXXXXXX,        KC_VOLU,                        KC_MPRV,        XXXXXXX,        KC_7,           KC_8,           KC_9,           KC_KP_PLUS,     _______,        
     _______,        KC_PERC,        KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_PIPE,        KC_VOLD,                        KC_MPLY,        XXXXXXX,        KC_4,           KC_5,           KC_6,           KC_KP_EQUAL,    _______,        
     _______,        KC_MINUS,       KC_UNDS,        KC_EQUAL,       KC_PLUS,        KC_BSLASH,                                                      XXXXXXX,        KC_1,           KC_2,           KC_3,           XXXXXXX,        _______,        
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_MOU_NAV] = LAYOUT_moonlander(
     TO(0),          KC_ACL0,        KC_ACL1,        KC_ACL2,        XXXXXXX,        XXXXXXX,        KC_MUTE,                        KC_MPRV,        KC_PAUSE,       KC_PSCREEN,     KC_INSERT,      KC_SCROLLLOCK,  _______,        _______,        
     _______,        KC_WH_L,        KC_WH_D,        KC_WH_U,        KC_WH_R,        XXXXXXX,        KC_VOLU,                        KC_MNXT,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        _______,        _______,        
-    KC_CAPSLOCK,    KC_MS_L,        KC_MS_D,        KC_MS_U,        KC_MS_R,        XXXXXXX,        KC_VOLD,                        KC_MPLY,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       _______,        _______,        
+    _______,        KC_MS_L,        KC_MS_D,        KC_MS_U,        KC_MS_R,        XXXXXXX,        KC_VOLD,                        KC_MPLY,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       _______,        _______,        
     _______,        KC_BTN1,        KC_BTN2,        KC_BTN3,        KC_BTN4,        KC_BTN5,                                                        KC_HOME,        KC_PGDOWN,      KC_PGUP,        KC_END,         _______,        _______,        
     _______,        MC_KVM1,        MC_KVM2,        XXXXXXX,        XXXXXXX,                        _______,                        _______,                        XXXXXXX,        XXXXXXX,        XXXXXXX,        _______,        _______,        
                                                                     _______,        _______,        _______,                        _______,        _______,        _______
@@ -131,7 +131,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
   ),
 
   [LAYER_SYM_NUM] = LEDMAP_moonlander(
-    WHITE, BLACK,  BLACK,  BLACK,   BLACK,  BLACK,  NAVY,        NAVY,   BLACK,  YELLOW, PURPLE, PURPLE, PURPLE, BLACK,    
+    WHITE, BLACK,  BLACK,  BLACK,   BLACK,  BLACK,  NAVY,        NAVY,   YELLOW, YELLOW, PURPLE, PURPLE, PURPLE, BLACK,    
     BLACK, PURPLE, PURPLE, PURPLE,  PURPLE, BLACK,  NAVY,        NAVY,   BLACK,  BLUE,   BLUE,   BLUE,   PURPLE, BLACK,    
     BLACK, PURPLE, PURPLE, PURPLE,  PURPLE, PURPLE, NAVY,        NAVY,   BLACK,  BLUE,   BLUE,   BLUE,   PURPLE, BLACK,    
     BLACK, PURPLE, PURPLE, PURPLE,  PURPLE, PURPLE,                      BLACK,  BLUE,   BLUE,   BLUE,   BLACK,  BLACK,    
@@ -142,7 +142,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
   [LAYER_MOU_NAV] = LEDMAP_moonlander(
     WHITE, LIME,   LIME,   LIME,    BLACK,  BLACK,  NAVY,        NAVY,   CYAN,   CYAN,   CYAN,   YELLOW, BLACK,  BLACK,
     BLACK, GREEN,  GREEN,  GREEN,   GREEN,  BLACK,  NAVY,        NAVY,   BLACK,  BLACK,  BLACK,  BLACK,  BLACK,  BLACK,
-    YELLOW,LIME,   LIME,   LIME,    LIME,   BLACK,  NAVY,        NAVY,   CYAN,   CYAN,   CYAN,   CYAN,   BLACK,  BLACK,
+    BLACK, LIME,   LIME,   LIME,    LIME,   BLACK,  NAVY,        NAVY,   CYAN,   CYAN,   CYAN,   CYAN,   BLACK,  BLACK,
     BLACK, GREEN,  GREEN,  GREEN,   GREEN,  GREEN,                       CYAN,   CYAN,   CYAN,   CYAN,   BLACK,  BLACK,
     BLACK, ORANGE, ORANGE, BLACK,   BLACK,          BLACK,       BLACK,          BLACK,  BLACK,  BLACK,  BLACK,  BLACK,
                                     PURPLE, LIME,   PURPLE,      LIME,   LIME,   PURPLE
